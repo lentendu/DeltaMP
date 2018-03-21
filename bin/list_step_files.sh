e699c2ffec20d8aed432c1c71863f3bd2b413be0
ahb-ufz <anna.heintz-buschart@ufz.de>
2018-03-21 17:47:33 +0100

    fixed weird names output be list_step_files and consequences

--- bin/list_step_files.sh
+++ bin/list_step_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-MY_NAME=`grep "N $JOB_NAME " $EXEC/config/steps.final | awk '{print $NF}'`
-find $EXEC/* -type d | sort > $EXEC/config/${MY_NAME##*/}.dir
-find $EXEC ! -type d | cat - <(echo $EXEC/config/${MY_NAME##*/}.files) | sort > $EXEC/config/${MY_NAME##*/}.files
+MY_TMP_NAME=`grep "N $JOB_NAME " $EXEC/config/steps.final | awk '{print $NF}'`
+MY_TMP_NAME2=${MY_TMP_NAME##*/}
+find $EXEC/* -type d | sort > $EXEC/config/${MY_TMP_NAME2%%)}.dir
+find $EXEC ! -type d | cat - <(echo $EXEC/config/${MY_TMP_NAME2%%)}.files) | sort > $EXEC/config/${MY_TMP_NAME2%%)}.files
